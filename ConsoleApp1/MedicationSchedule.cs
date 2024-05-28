dc9eef65d8767379d9bb8a8bf5014320be240988
Hilal Nur DURDAÇ <durdachilalnur@gmail.com>
2024-05-28 11:24:48 +0300

    [Feat] all day medicine add

---
+++ ConsoleApp1/MedicationSchedule.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+
+    class MedicationSchedule
+    {
+        public string Name { get; set; }
+        public int DailyDoses { get; set; }
+        public bool IsAfterMeal { get; set; }
+        public string Condition { get; set; }
+        public bool DissolveInWater { get; set; }
+
+        public MedicationSchedule(string name, int dailyDoses, bool isAfterMeal, string condition, bool dissolveInWater = false)
+        {
+            Name = name;
+            DailyDoses = dailyDoses;
+            IsAfterMeal = isAfterMeal;
+            Condition = condition;
+            DissolveInWater = dissolveInWater;
+        }
+
+        public void TakeMedication()
+        {
+            for (int dose = 1; dose <= DailyDoses; dose++)
+            {
+                string message = $"İlaç {Name} doz {dose}: ";
+                if (DissolveInWater)
+                {
+                    message += "suda eritilerek ";
+                }
+
+                message += Condition == "aç" ? "aç karnına " : "tok karnına ";
+                message += "alınacak.";
+                
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,35 @@
 ﻿
+using ConsoleApp1;
+
+List<MedicationSchedule> medications = new List<MedicationSchedule>
+            {
+                new MedicationSchedule("A", 2, true, "tok"),
+                new MedicationSchedule("B", 3, true, "tok"),
+                new MedicationSchedule("C", 3, true, "tok"),
+                new MedicationSchedule("D", 1, false, "tok", true),
+                new MedicationSchedule("E", 3, false, "aç")
+            };
+
+            Console.WriteLine("Bir haftalık ilaç programı başlıyor...");
+
+            for (int day = 1; day <= 7; day++)
+            {
+                Console.WriteLine($"Gün {day}:");
+                foreach (var med in medications)
+                {
+                    med.TakeMedication();
+                }
+            }
+
+            Console.Write("Bir hafta sonunda ilaçlara devam etmek istiyor musunuz? (E/H): ");
+            string response = Console.ReadLine().ToUpper();
+
+            if (response == "E")
+            {
+                Console.WriteLine("İlaçlara devam ediyorsunuz.");
+            }
+            else
+            {
+                Console.WriteLine("İlaçları bırakıyorsunuz.");
+            }
+    
